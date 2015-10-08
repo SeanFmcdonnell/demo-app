47fc996531ca7e22992149c80fa3e59ecb21fe59
Connor Kuhn <connor.kuhn@finicity.com>
2015-10-07 18:31:32 -0600

    Added both customer ids as keys for EventOutput
    Only one customer id was being added to the map for EventOutputs
    Now both are added as different keys referencing the same Event

--- app/src/main/java/com/finicity/example/resources/SubscriptionResource.java
+++ app/src/main/java/com/finicity/example/resources/SubscriptionResource.java
@@ -1,9 +1,7 @@
 package com.finicity.example.resources;
 
 import com.finicity.client.FinicityClient;
-import com.finicity.client.models.Subscriptions;
-import com.finicity.client.models.Event;
-import com.finicity.client.models.Transaction;
+import com.finicity.client.models.*;
 import com.finicity.example.api.User;
 import com.finicity.example.services.AuthService;
 import com.google.api.client.util.Maps;
@@ -21,8 +19,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriInfo;
 import java.io.IOException;
 import java.net.URI;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 /**
  * Created by jhutchins on 9/29/15.
@@ -45,8 +42,8 @@ public class SubscriptionResource {
             @PathParam("type") final String type,
             String accountId,
             @Context UriInfo info) {
-        URI callback = info.getRequestUriBuilder()
-                .path("callback")
+        URI callback = info.getBaseUriBuilder()
+                .path("api/subscriptions/callback")
                 .build();
         return client.createSubscription(user.getId(type), accountId, callback.toASCIIString());
     }
@@ -55,22 +52,32 @@ public class SubscriptionResource {
     @Path("callback")
     @Consumes(MediaType.APPLICATION_XML)
     public void callback(Event event) {
-
         event.getTransaction().stream()
                 .filter(transaction -> outputs.get(transaction.getCustomerId()) != null)
                 .map(transaction -> {
-                    log.info(transaction.toString());
                     EventOutput output = outputs.get(transaction.getCustomerId());
-                    try {
-                        output.write(new OutboundEvent.Builder()
-                                .mediaType(MediaType.APPLICATION_JSON_TYPE)
-                                .name("transaction")
-                                .data(transaction)
-                                .build());
-                        log.info("Wrote transaction [{}]", transaction);
-                    } catch (IOException e) {
-                        log.error("Error sending event", e);
-                    }
+                    List<Account> accounts = client.getCustomerAccounts(transaction.getCustomerId()).getList();
+                    accounts.stream()
+                            .filter(account -> account.getId().equals(transaction.getAccountId()))
+                            .map(account -> {
+                                log.info(account.getName());
+                                transaction.setAccountName(account.getName());
+                                return transaction;
+                            })
+                            .distinct()
+                            .forEach(t -> {
+                                try {
+                                    output.write(new OutboundEvent.Builder()
+                                            .mediaType(MediaType.APPLICATION_JSON_TYPE)
+                                            .name("transaction")
+                                            .data(t)
+                                            .build());
+                                    log.info("Wrote transaction [{}]", t.toString());
+                                } catch (IOException e) {
+                                    log.error("Error sending event", e);
+                                }
+
+                            });
                     return output;
                 })
                 .distinct()
@@ -87,16 +94,20 @@ public class SubscriptionResource {
     @Path("events")
     @Produces(SseFeature.SERVER_SENT_EVENTS)
     public EventOutput getServerSentEvents(@QueryParam("token") String token) {
+        final User user = auth.getUser(token);
         final EventOutput eventOutput = new EventOutput();
-        User user = auth.getUser(token);
-        Optional.ofNullable(outputs.get(user.getId("active"))).ifPresent(output -> {
-            try {
-                output.close();
-            } catch (IOException e) {
-                log.error("Error closing output", e);
-            }
-        });
-        outputs.put(user.getId("active"), eventOutput);
+
+        for (String type : new String[]{"active", "testing"}) {
+            final String id = user.getId(type);
+            Optional.ofNullable(outputs.get(id)).ifPresent(output -> {
+                try {
+                    output.close();
+                } catch (IOException e) {
+                    log.error("Error closing output", e);
+                }
+            });
+            outputs.put(id, eventOutput);
+        }
         return eventOutput;
     }
 
